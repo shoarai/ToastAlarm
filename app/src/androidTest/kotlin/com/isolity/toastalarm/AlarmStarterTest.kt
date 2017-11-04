36391941cb505995e4064f8a89c4f4f2635a5a47
shoarai <shoarai@users.noreply.github.com>
2017-11-04 12:07:17 +0900

    Fix test build error by set maven repository

---
+++ app/src/androidTest/kotlin/com/isolity/toastalarm/AlarmStarterTest.kt
@@ -0,0 +1,28 @@
+package com.isolity.toastalarm
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.assertEquals
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class AlarmStarterTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+
+        assertEquals("com.isolity.toastalarm", appContext.packageName)
+    }
+}
---
+++ app/src/main/kotlin/com/isolity/toastalarm/AlarmService.kt
@@ -0,0 +1,7 @@
+package com.isolity.toastalarm
+
+/**
+ * Created by shohei52a on 2017/11/04.
+ */
+class AlarmServuce {
+}
