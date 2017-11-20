f8de52406801f12eb24eef84dd74a1722773d434
shoarai <shoarai@users.noreply.github.com>
2017-11-20 20:38:27 +0900

    Fix cast error in preference data

--- app/src/main/kotlin/com/isolity/toastalarm/repository/WeeklyAlarmRepository.kt
+++ app/src/main/kotlin/com/isolity/toastalarm/repository/WeeklyAlarmRepository.kt
@@ -1,8 +1,11 @@
 package com.isolity.toastalarm.repository
 
 import android.content.Context
+import android.preference.PreferenceManager
 import android.util.Log
+import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
+import com.google.gson.reflect.TypeToken
 import com.isolity.toastalarm.model.WeeklyAlarm
 
 
@@ -20,8 +23,23 @@ object WeeklyAlarmRepository {
      * @return weekly alarms
      */
     fun getAll(context: Context): Array<WeeklyAlarm>? {
+//        return try {
+//            preference.restore(context)
+//        } catch (e: JsonSyntaxException) {
+//            Log.v(TAG, e.toString())
+//            null
+//        }
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val stringData = prefs.getString(storageKey, "")
+        if (stringData.isBlank()) {
+            return null
+        }
+
         return try {
-            preference.restore(context)
+            val gson = Gson()
+            val type = object : TypeToken<Array<WeeklyAlarm>>() {}.type
+            return gson.fromJson<Array<WeeklyAlarm>>(stringData, type)
         } catch (e: JsonSyntaxException) {
             Log.v(TAG, e.toString())
             null
@@ -33,6 +51,11 @@ object WeeklyAlarmRepository {
      * @param weeklyAlarms weeklyAlarms to store
      */
     fun update(context: Context, weeklyAlarms: Array<WeeklyAlarm>) {
-        preference.save(context, weeklyAlarms)
+//        preference.save(context, weeklyAlarms)
+
+        val gson = Gson()
+        val jsonInstanceString = gson.toJson(weeklyAlarms)
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        prefs.edit().putString(storageKey, jsonInstanceString).apply()
     }
 }
